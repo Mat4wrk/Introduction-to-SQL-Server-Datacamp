94d19118fcaa98c302a401a5159686773f62cc8f
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-16 18:34:14 +0700

    Create Declare multiple variables.sql

---
+++ 4.You've got the power/Declare multiple variables.sql
@@ -0,0 +1,33 @@
+-- Declare @start
+DECLARE @start DATE
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24';
+
+-- Declare @stop
+DECLARE @stop DATE
+-- SET @stop to '2014-07-02'
+SET @stop = '2014-07-02'
+
+-- Declare @affected
+DECLARE @affected INT
+-- Set @affected to 5000
+SET @affected = 5000
+
+
+-- Declare @start
+DECLARE @start DATE
+
+-- Declare @stop
+DECLARE @stop DATE
+
+-- Declare @affected
+DECLARE @affected INT
+
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24'
+
+-- SET @stop to '2014-07-02'
+SET @stop  = '2014-07-02'
+
+-- Set @affected to 5000
+SET @affected = 5000
